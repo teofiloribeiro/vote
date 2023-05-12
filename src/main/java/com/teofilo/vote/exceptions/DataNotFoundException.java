5e71a4862ba662426835d24e75f117a82848d176
Teofilo Ribeiroe <teofilo_ribeiro@hotmail.com>
2023-05-12 10:13:42 -0300

    Creation DataNotFoundException
    Exception created to signal when some type of data is not found

---
+++ src/main/java/com/teofilo/vote/exceptions/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.teofilo.vote.exceptions;
+
+public class DataNotFoundException extends Exception{
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
+
