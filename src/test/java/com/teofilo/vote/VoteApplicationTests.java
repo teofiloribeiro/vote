1ee714390ad9116b17525f38cab7d8a741883093
Teofilo Ribeiroe <teofilo_ribeiro@hotmail.com>
2023-05-11 22:41:49 -0300

    Initial Commit

---
+++ src/test/java/com/teofilo/vote/VoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teofilo.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
